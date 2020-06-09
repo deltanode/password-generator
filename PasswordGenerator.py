fd6019e9819137a778c438fdb4039762655c3795
Yogesh Yadav <yogesh7132@gmail.com>
2020-06-09 11:16:01 +0530

    Added GUI

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,5 +1,6 @@
 import string
 import random
+from tkinter import *
 
 def get_pwd(len):
 
@@ -16,7 +17,9 @@ def get_pwd(len):
 
     random.shuffle(lst)
     pwd = "".join(lst)
-    print(pwd)
+    # print(pwd)
+    display_textbox.insert(INSERT,pwd)
+
 
 def chk_input(len):
     if (len < 8):
@@ -26,10 +29,43 @@ def chk_input(len):
         get_pwd(len)
 
 def get_input():
-    len=int(input("Enter Password Length : "))
+    # len=int(input("Enter Password Length : "))
+    len=int(inpEntry.get())
     chk_input(len)
 
-if __name__ == "__main__":
-    get_input()
+# if __name__ == "__main__":
+    # get_input()
+    # pass
+
+def reset():
+    display_textbox.delete(1.0, END)
+
+## Creating GUI
+
+root = Tk()
+
+root.title("Password Generator - [YYScoop.com]")
+root.geometry("400x400")
+
+fontstyle=("Helvitica",10,"bold")
+txt_fontstyle=("Helvitica",15,"bold")
+inplabel = Label(root,text="Enter the Digit of Password:", font= fontstyle)
+inplabel.grid(row=0,column=0,columnspan=2,sticky=W,padx=10, pady=5)
+
+inpEntry = Entry(root,width=33,font=txt_fontstyle)
+inpEntry.grid(row=1,column=0,columnspan=2,sticky=W,padx=10, pady=5)
+
+submit_btn = Button(root,text="Generate Password", command=get_input,fg="white",bg="#f57971",width=22)
+submit_btn.grid(row=2,column=0,columnspan=2,sticky=W,padx=10, pady=5)
+
+reset_btn = Button(root,text="Reset All ", command=reset,fg="white",bg="green",width=22)
+reset_btn.grid(row=2,column=1,columnspan=2,sticky=W,padx=10, pady=5)
 
 
+outlabel = Label(root,text="Your secure password is:", font= fontstyle)
+outlabel.grid(row=3,column=0,columnspan=2,sticky=W,padx=10, pady=5)
+
+display_textbox = Text(root,font=txt_fontstyle, width=33, height = 10)
+display_textbox.grid(row=4,column=0,columnspan=2,sticky=W,padx=10, pady=5)
+
+root.mainloop()
